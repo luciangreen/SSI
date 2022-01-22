1e985ae804674f9517cebf10ad48fe74c23baebc
Lucian Green <luciangreen@lucianacademy.com>
2022-01-22 23:12:49 +1100

    Update ssi3.pl

--- ssi3.pl
+++ ssi3.pl
@@ -2,7 +2,7 @@
 
 
 /*
-see lpi test 15 for swipl calll with enough memory for test 15
+see lpi test 15 for swipl call with enough memory for test 15
 
 numbers(194,1,[],N),findall(N1,(member(N1,N),test(N1,Q,F,R),catch(call_with_time_limit(4,lucianpl(off,Q,F,R)),_,false),writeln(N1)),N2),sort(N2,N3),writeln(N3),length(N3,L),subtract(N,N3,U),writeln(U).
 
