3fe0130ee57b2e0f94d7e8230ec3861db25af406
Lucian Green <luciangreen@lucianacademy.com>
2023-11-22 00:04:13 +1100

    member2(List,Item) for CAW.

--- ssi_listrecursion4.pl
+++ ssi_listrecursion4.pl
@@ -270,12 +270,12 @@ interpretstatement1(ssi,_F0,_Functions,[[Dbw_n,Dbw_member2],[Variable2,Variable1
 get_lang_word("n",Dbw_n1),Dbw_n1=Dbw_n,
 %trace,
 ((get_lang_word("member2",Dbw_member21),
-Dbw_member21=Dbw_member2)->true;
+Dbw_member21=Dbw_member2)->        ssi_interpretpart(member2,Dbw_member2,Variable2,Variable1,Vars1,Vars2,Vars2c);
 (get_lang_word("member",Dbw_member22),
-Dbw_member22=Dbw_member2)),
+Dbw_member22=Dbw_member2,
 %%writeln1(8),
 %trace,
-        ssi_interpretpart(member2,Dbw_member2,Variable1,Variable2,Vars1,Vars2,Vars2c).
+        ssi_interpretpart(member2,Dbw_member2,Variable1,Variable2,Vars1,Vars2,Vars2c))).
 
 interpretstatement1(ssi,_F0,_Functions,[[Dbw_n,Dbw_member3],[Variable1,Variable2]],Vars1,Vars2,true,nocut,Vars2c) :-
 get_lang_word("n",Dbw_n1),Dbw_n1=Dbw_n,
